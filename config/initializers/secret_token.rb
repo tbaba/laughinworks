5f94abeab04f82ace351445c00d883a4a8d2eadc
Tatsuro BABA <harakirisoul@gmail.com>
2011-02-11 01:18:26 +0900

    rails new laughinworks -JT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laughinworks::Application.config.secret_token = 'a56685945fea4784ac16f603bafefcdb768702cede3ddb6d80799caebc05eda268e6ef7b3245d3f070a936eb97562622bc5927e82bdaba602e9e7f9307d16a4c'
