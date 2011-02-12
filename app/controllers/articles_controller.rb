6b8a99ac60dd8bb528819edf29747c06d912f178
Tatsuro BABA <harakirisoul@gmail.com>
2011-02-13 00:09:36 +0900

    resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.limit(10).order('created_at desc')
   end
 
   def show
+    @article = Article.find(params[:id])
   end
-
 end
